53d6abe625bf07e2078f0e277b536661f1db5f1c
takaakikakuta <soci0926@gmail.com>
2021-07-09 14:00:35 +0900

    learn PropsType

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 function App() {
   return (
     <div>
-      <User name={"Taro"}/>
+      <User name={"NoName"}/>
     </div>
   );
 }
 
 const User = (props) => {
   return <div>Hi, I am {props.name} </div>
+}
+
+User.prototype = {
+  name: PropTypes.string
 }
 
 export default App;
