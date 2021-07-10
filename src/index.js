0c844c72cc79ee03a76a6f1c6fba2bd37bfa33c2
takaakikakuta <soci0926@gmail.com>
2021-07-10 11:01:21 +0900

    reducer end

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import './index.css';
 import reducer from './reducers';
-import App from './App';
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
 const store = createStore(reducer)
